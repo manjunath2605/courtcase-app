347619adf05850e88efe926bbecd4ecb0be1bab9
Manjunath Ingale <manjuingale00@gmail.com>
2025-12-15 23:08:56 +0530

    Add token related changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://courtcase-app.onrender.com/api"
+ // baseURL: "https://courtcase-app.onrender.com/api"
+  baseURL: "http://localhost:5000/api"
+
 });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = token;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
