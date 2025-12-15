dc31ef85863387838966549a10e7ef1be228285b
Manjunath Ingale <manjuingale00@gmail.com>
2025-12-15 23:09:20 +0530

    Add token related changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- // baseURL: "https://courtcase-app.onrender.com/api"
-  baseURL: "http://localhost:5000/api"
-
+  baseURL: "https://courtcase-app.onrender.com/api"
 });
 
 api.interceptors.request.use(config => {
